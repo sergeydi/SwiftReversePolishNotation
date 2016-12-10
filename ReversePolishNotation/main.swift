11b1cc4377a52a91af4d25a1351956195b119826
Sergey Didanov <svdidanov@icloud.com>
2016-12-10 21:03:21 +0200

    Initial Commit

---
+++ ReversePolishNotation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReversePolishNotation
+//
+//  Created by Sergey Didanov on 10.12.16.
+//  Copyright © 2016 Sergey Didanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
